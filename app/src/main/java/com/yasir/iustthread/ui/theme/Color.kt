d6473a34fee304b370b65ee67304547fe7e4ffc4
Yasir alam <yasiralam981@gmail.com>
2025-06-27 15:35:04 +0530

    [✨] fixes the add thread screen and added icons

--- app/src/main/java/com/yasir/iustthread/ui/theme/Color.kt
+++ app/src/main/java/com/yasir/iustthread/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PinkColor = Color(0xFFE91E63)
+val LightBorderGray = Color.Gray.copy(alpha = 0.1f)
+val TextGray = Color.Gray
+val gray = Color(0xFF6B7280)
