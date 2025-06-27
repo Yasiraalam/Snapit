13f4ff106703a29c9f26d5c17c609e16c468d7bc
Yasir alam <yasiralam981@gmail.com>
2025-06-27 15:59:35 +0530

    [✨] added some colors

--- app/src/main/java/com/yasir/iustthread/ui/theme/Color.kt
+++ app/src/main/java/com/yasir/iustthread/ui/theme/Color.kt
@@ -14,3 +14,9 @@ val PinkColor = Color(0xFFE91E63)
 val LightBorderGray = Color.Gray.copy(alpha = 0.1f)
 val TextGray = Color.Gray
 val gray = Color(0xFF6B7280)
+val PinkBackground = Color(0xFFFFF1F5)
+val LightGrey = Color(0xFFF5F5F5)
+val TextGrey = Color.Gray
+val TextLightGrey = Color.LightGray
+val BlueAccent = Color(0xFF2196F3)
+val GreenAccent = Color(0xFF4CAF50)
